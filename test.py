56cff572a262024d34e6db5e7b4289840b7ee6c6
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-06-28 19:48:46 +1000

    Implemented randomness and better method decleration Need ideas to improve

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 import matplotlib.pyplot as plt
 
 from collections import OrderedDict
@@ -12,17 +14,17 @@ from sklearn.ensemble import RandomForestClassifier
 
 print(__doc__)
 
-RANDOM_STATE = 123
-
+RANDOM_STATE = random.randint(100,200)
 # Generate a binary classification dataset.
 X, y = make_classification(n_samples=500, n_features=25,
                            n_clusters_per_class=1, n_informative=15,
                            random_state=RANDOM_STATE)
 
+
 # NOTE: Setting the `warm_start` construction parameter to `True` disables
 # support for parallelized ensembles but is necessary for tracking the OOB
 # error trajectory during training.
-ensemble_clfs = [
+ensemble_clfs = (
     ("RandomForestClassifier, max_features='sqrt'",
      RandomForestClassifier(warm_start=True, oob_score=True,
                             max_features="sqrt",
@@ -35,17 +37,17 @@ ensemble_clfs = [
      RandomForestClassifier(warm_start=True, max_features=None,
                             oob_score=True,
                             random_state=RANDOM_STATE))
-]
+)
 
 # Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
 error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
 
 # Range of `n_estimators` values to explore.
-min_estimators = 15
-max_estimators = 175
+MIN_ESTIMATORS = 50
+MAX_ESTIMATORS = 175
 
 for label, clf in ensemble_clfs:
-    for i in range(min_estimators, max_estimators + 1):
+    for i in range(MIN_ESTIMATORS, MAX_ESTIMATORS + 1):
         clf.set_params(n_estimators=i)
         clf.fit(X, y)
 
@@ -58,7 +60,7 @@ for label, clf_err in error_rate.items():
     xs, ys = zip(*clf_err)
     plt.plot(xs, ys, label=label)
 
-plt.xlim(min_estimators, max_estimators)
+plt.xlim(MIN_ESTIMATORS, MAX_ESTIMATORS)
 plt.xlabel("n_estimators")
 plt.ylabel("OOB error rate")
 plt.legend(loc="upper right")
